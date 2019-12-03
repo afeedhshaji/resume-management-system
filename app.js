2371225fd1e3a63dca03ba83ee808652139e5080
Afeedh Shaji <afeedh_b170282cs@nitc.ac.in>
2019-12-03 22:00:48 +0530

    Autocomplete bug needs to be fixed

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
 const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+// connect to db
 mongoose
   .connect(process.env.DB_CONNECT, {
     useUnifiedTopology: true,
@@ -15,16 +18,17 @@ mongoose
   .then(() => console.log('Now connected to MongoDB!'))
   .catch(err => console.error(`Something went wrong ${err}`));
 
-// For parsing directories
-const path = require('path');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// Template engine - temp
-app.set('view engine', 'jade');
-
 app.get('/', function(req, res) {
   res.render('index', { title: 'Home Page' });
+});
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// index page
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 // middleware
@@ -33,4 +37,4 @@ app.use(express.json());
 const authCandidate = require('./routes/auth');
 app.use('/api/candidate', authCandidate);
 
-app.listen(5000, () => console.log('Server up and running'));
+app.listen(8000, () => console.log('Server up and running'));
