bc7f7d05ee21a6737845b7d6bd9d9dfa08bc2e41
abinpaul1 <abin.paul1@gmail.com>
2019-12-03 14:06:00 +0530

    Basic directory struture---Jade--temporary engine

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const path = require('path');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//Template engine
-//app.set('view engine', 'jade'); 
+//Template engine - temp
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('index', { title: 'Home Page' } );
+});
 
 app.listen(3000, () => console.log('Server up and running'));
