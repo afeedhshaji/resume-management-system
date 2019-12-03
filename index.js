7246d399637b99ee547de46bbf3126e73e79878c
Afeedh Shaji <afeedh_b170282cs@nitc.ac.in>
2019-12-03 12:38:01 +0530

    Added eslint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log("Server up and running"));
+app.listen(3000, () => console.log('Server up and running'));
