cf44e5fc63955c48dec94111d3991180cb431daf
abinpaul1 <abin.paul1@gmail.com>
2019-12-06 01:54:29 +0530

    Commented out logging

--- routes/autocomplete.js
+++ routes/autocomplete.js
@@ -4,12 +4,12 @@ const Skills = require('../models/skills');
 
 //Autocomplete position API
 router.get('/list/autocomplete/position', function(req, res, next) {
-  console.log('entered');
+  // console.log('entered');
   const regex = new RegExp(req.query['term'], 'i');
-  console.log('entered');
+  // console.log('entered');
   const userFilter = Position.find({ position: regex }, { position: 1 }).limit(20);
   userFilter.exec(function(err, data) {
-    console.log(data);
+    // console.log(data);
     const result = [];
     if (!err) {
       if (data && data.length && data.length > 0) {
@@ -21,7 +21,7 @@ router.get('/list/autocomplete/position', function(req, res, next) {
           result.push(obj);
         });
       }
-      console.log(result);
+      // console.log(result);
       res.jsonp(result);
     }
   });
@@ -29,12 +29,12 @@ router.get('/list/autocomplete/position', function(req, res, next) {
 
 //Autocomplete skills API
 router.get('/list/autocomplete/skills', function(req, res, next) {
-  console.log('entered');
+  // console.log('entered');
   const regex = new RegExp(req.query['term'], 'i');
-  console.log('entered');
+  // console.log('entered');
   const userFilter = Skills.find({ skill: regex }, { skill: 1 }).limit(20);
   userFilter.exec(function(err, data) {
-    console.log(data);
+    // console.log(data);
     const result = [];
     if (!err) {
       if (data && data.length && data.length > 0) {
@@ -46,7 +46,7 @@ router.get('/list/autocomplete/skills', function(req, res, next) {
           result.push(obj);
         });
       }
-      console.log(result);
+      // console.log(result);
       res.jsonp(result);
     }
   });
