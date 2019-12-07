aa55951ddfc8773c403e5f22386866e9d94ebf41
Afeedh Shaji <afeedh_b170282cs@nitc.ac.in>
2019-12-07 21:29:59 +0530

    Integrated qualification autocomplete feature

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -110,4 +110,27 @@ $(document).ready(function() {
       }
     }
   });
+  $('#search-qualification').autocomplete({
+    source: function(req, res) {
+      $.ajax({
+        url: '/api/candidate/list/autocomplete/qualification',
+        dataType: 'jsonp',
+        type: 'GET',
+        data: req,
+        success: function(data) {
+          res(data);
+        },
+        error: function(err) {
+          console.log(err.status);
+        }
+      });
+    },
+
+    minLength: 1,
+    select: function(event, ui) {
+      if (ui.items) {
+        $('#search-qualification').text(ui.item.label);
+      }
+    }
+  });
 });
--- public/scripts/register.js
+++ public/scripts/register.js
@@ -142,6 +142,30 @@ $(document).ready(function() {
     }
   });
 
+  $('#qualification').autocomplete({
+    source: function(req, res) {
+      $.ajax({
+        url: '/api/candidate/list/autocomplete/qualification',
+        dataType: 'jsonp',
+        type: 'GET',
+        data: req,
+        success: function(data) {
+          res(data);
+        },
+        error: function(err) {
+          console.log(err.status);
+        }
+      });
+    },
+
+    minLength: 1,
+    select: function(event, ui) {
+      if (ui.items) {
+        $('#search-qualification').text(ui.item.label);
+      }
+    }
+  });
+
   // Position Autocomplete
   $('#position').autocomplete({
     source: function(req, res) {
