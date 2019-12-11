871f13ffcdf42ea8ff72e0028d573786b1d9cdf8
Afeedh Shaji <afeedh_b170282cs@nitc.ac.in>
2019-12-11 12:22:01 +0530

    Added gulp browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const nodemon = require('gulp-nodemon');
+
+gulp.task('default', ['browser-sync'], function() {});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: 'http://localhost:3035',
+    files: ['public/**/*.*'],
+    browser: 'google chrome',
+    port: 7000
+  });
+});
+gulp.task('nodemon', function(cb) {
+  const started = false;
+
+  return nodemon({
+    script: 'app.js'
+  }).on('start', function() {
+    // to avoid nodemon being started multiple times
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
