27c16ff3e18b446f450e1fe5ea8099c9b83cea1b
abinpaul1 <abin.paul1@gmail.com>
2019-12-17 19:26:24 +0530

    bug fix for location autocomplete

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -154,6 +154,9 @@ $(document).ready(function() {
         type: 'GET',
         data: req,
         success: function(data) {
+          for (var i = 0; i<data.length; i=i+1){
+            data[i]['label'] = data[i]['label'].replace(/\b\w+/g,function(s){return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();});
+          }
           res(data);
         },
         error: function(err) {
