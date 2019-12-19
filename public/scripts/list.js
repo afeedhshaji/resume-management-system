293ef83edb42d0860d56a85610b18ed3038f9118
abinpaul1 <abin.paul1@gmail.com>
2019-12-19 16:16:11 +0530

    Added functionality to copy selected emails to clipboard

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -189,5 +189,94 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop:0}, '300');
   });
 
+  $('#select-all').click( function () {
+    checkboxes = document.getElementsByClassName("row-checkbox");
+    console.log(checkboxes.length)
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+      checkboxes[i].checked = this.checked;
+    }
+  });
 
 });
+
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a
+  // flash, so some of these are just precautions. However in
+  // Internet Explorer the element is visible whilst the popup
+  // box asking the user for permission for the web page to
+  // copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copyied successfully');
+  } catch (err) {
+    console.log('Oops, unable to copy. Try upgrading browser');
+  }
+
+  document.body.removeChild(textArea);
+}
+
+
+//Checkbox function to copy emails of slected rows
+function GetSelectedEmails() {
+      //Reference the Table.
+      var grid = document.getElementById("list-candidates");
+
+      //Reference the CheckBoxes in Table.
+      var checkBoxes = grid.getElementsByTagName("INPUT");
+      message = "";
+
+      //Loop through the CheckBoxes.
+      for (var i = 1; i < checkBoxes.length; i++) {
+          if (checkBoxes[i].checked) {
+              var row = checkBoxes[i].parentNode.parentNode;
+              message += row.cells[14].innerHTML;
+              message += " ";
+          }
+      }
+
+      copyTextToClipboard(message);
+      alert("Copied email of selected rows");
+}
